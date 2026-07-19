87eb4e179efc9bb8942266dc29de83638c0c18f4
javinsingh <javinsinghsethi@gmail.com>
2026-07-19 18:01:29 +0200

    Add files via upload

--- simplechatbot.py
+++ simplechatbot.py
@@ -10,5 +10,16 @@ elif mood == ' ':
     print("I get that. Sometimes feelings can't be put in words.")
 else:
     print("im not sure what you are telling me.")
+print("what are your hobbies?")
+hobbies = input()
+print(f"Oh nice! {hobbies} is very fun isn't it!")
+print("how old are you?")
+age = input(int)
+if age < 18:
+    print(f"Nice! you are still a child, you have got a lot to learn at {age}")
+elif age < 18 and age > 40:
+    print(f"Nice! You are still very young at the age of {age}, but you are growing!")
+elif age > 40:
+    print(f"Nice! People at the age of {age} have a lot of experience!")
 print(f"it was great talking to you {name}, Bye!")
 
